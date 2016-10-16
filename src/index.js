03eeab79781b288d0bab516b7c6ff143165c039b
limeng32 <limeng32@live.cn>
2016-10-16 16:22:39 +0800

    Init project.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+KISSY.use('awarding-account-front/index.css', function (S) {
+//初始化header模块
+var header = require('./header/header');
+header.init();
+
+//初始化article模块
+var article = require('./article/article');
+article.init();
+})
