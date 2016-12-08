46cdd98d3348ed2536b07488c2200fa199f75576
limeng32 <limeng32@live.cn>
2016-12-08 18:21:36 +0800

    加入了进度条模块。

--- src/projectPage/article.js
+++ src/projectPage/article.js
@@ -1,25 +1,27 @@
-var $ = require('node').all;
-var XTemplate = require("kg/xtemplate/3.3.3/runtime");
-var Node = require('node');
-var Slide = require('kg/slide/2.0.2/');
-var Cutter = require('kg/cutter/2.0.0/');
-var SP = require('core-front/smartPath/smartPath');
-var IO = require('io');
-var JSONX = require('core-front/jsonx/jsonx');
-var OVL = require('overlay');
-var Auth = require('kg/auth/2.0.6/');
-var AuthMsgs = require('kg/auth/2.0.6/plugin/msgs/');
-var RAN = require('core-front/random/index');
-var SP = require('core-front/smartPath/smartPath');
-var AD = require('kg/agiledialog/1.0.2/index');
-var epTpl = require('./editProject/editProject-view');
-var editProject = require('./editProject/editProject');
+var $ = require('node').all
+var XTemplate = require("kg/xtemplate/3.3.3/runtime")
+var Node = require('node')
+var Slide = require('kg/slide/2.0.2/')
+var Cutter = require('kg/cutter/2.0.0/')
+var SP = require('core-front/smartPath/smartPath')
+var IO = require('io')
+var JSONX = require('core-front/jsonx/jsonx')
+var OVL = require('overlay')
+var Auth = require('kg/auth/2.0.6/')
+var AuthMsgs = require('kg/auth/2.0.6/plugin/msgs/')
+var RAN = require('core-front/random/index')
+var SP = require('core-front/smartPath/smartPath')
+var AD = require('kg/agiledialog/1.0.2/index')
+var epTpl = require('./editProject/editProject-view')
+var editProject = require('./editProject/editProject')
 var sideBar = require('../homePage/sideBar/sideBar')
-var subMenu = require('./subMenu/subMenu');
+var subMenu = require('./subMenu/subMenu')
+var stepBar = require('./stepBar/stepBar')
 module.exports = {
     init: function () {
         editProject.init({account:null})
         sideBar.init({})
         subMenu.init({})
+        stepBar.init({})
     }
 }
---
+++ src/projectPage/stepBar/stepBar.js
@@ -0,0 +1,36 @@
+var $ = require('node').all
+var XTemplate = require("kg/xtemplate/3.3.3/runtime")
+var Node = require('node')
+var OVL = require('overlay')
+var IO = require('io')
+var STB = require('kg/stepbar/2.1.0/index')
+var SP = require('core-front/smartPath/smartPath')
+var JSONX = require('core-front/jsonx/jsonx')
+var stbTpl = require('./stepBar-view')
+module.exports = {
+    init: function (p) {
+        var renderStep = function (p) {
+            var step = new STB('#projectPhase', {'act': 1, 'color': 'blue'})
+            step.render()
+        }
+        var stbHtml = new XTemplate(stbTpl).render({})
+        var ol = new OVL({
+            effect: 'slide',
+            easing: 'linear',
+            duration: 10,
+            target: '',
+            content: stbHtml,
+            visible: true,
+            xy: [25, 115],
+            width: '0px',
+            height: '0px',
+            closable: false,
+            zIndex: -1,
+            visible: true,
+            prefixCls: 'absolute-',
+            closeAction: 'hide'
+        })
+        ol.render()
+        renderStep()
+    }
+}
