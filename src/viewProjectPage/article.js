d4b69e0c8343e72a4f7d5d9dc8afdc3ac95b332b
limeng32 <limeng32@live.cn>
2016-12-23 11:15:33 +0800

    将stepBar放置于container中

--- src/viewProjectPage/article.js
+++ src/viewProjectPage/article.js
@@ -6,11 +6,28 @@ var IO = require('io')
 var JSONX = require('core-front/jsonx/jsonx')
 var OVL = require('overlay')
 var AD = require('kg/agiledialog/1.0.2/index')
+var STB = require('kg/stepbar/2.1.0/index')
 var containerView = require('./container-view')
 module.exports = {
     init: function () {
         var containerTpl = new XTemplate(containerView)
         var containerHtml = containerTpl.render({})
         $('article').html(containerHtml)
+        var _step
+        var _relation = $({
+            editing: 1
+            , submited: 2
+            , checking: 3
+            , pass: 4
+        })
+        var renderStep = function (p) {
+            _step = new STB('#projectPhase', {'color': 'blue'})
+            _step.render()
+        }
+        KISSY.use('kg/stepbar/2.1.0/stepbar.css', function (KISSY) {
+            KISSY.use('awarding-account-front/projectPage/stepBar/stepBar.css', function (KISSY) {
+                renderStep()
+            })
+        })
     }
 }
