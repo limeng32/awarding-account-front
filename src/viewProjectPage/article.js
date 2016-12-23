57a6e7b64eb69a0bffa538438be36cfe121b6df1
limeng32 <limeng32@live.cn>
2016-12-23 10:34:18 +0800

    在项目浏览页面中，做一个顶宽剧中容器container

--- src/viewProjectPage/article.js
+++ src/viewProjectPage/article.js
@@ -6,8 +6,11 @@ var IO = require('io')
 var JSONX = require('core-front/jsonx/jsonx')
 var OVL = require('overlay')
 var AD = require('kg/agiledialog/1.0.2/index')
+var containerView = require('./container-view')
 module.exports = {
     init: function () {
-        $('article').html('asd')
+        var containerTpl = new XTemplate(containerView)
+        var containerHtml = containerTpl.render({})
+        $('article').html(containerHtml)
     }
 }
