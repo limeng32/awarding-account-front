66e5fd5a89c05086fe85ccdf5f94097743d0ddc0
limeng32 <limeng32@live.cn>
2016-12-23 14:26:25 +0800

    在viewProject中可以看到stepBar

--- src/viewProjectPage/article.js
+++ src/viewProjectPage/article.js
@@ -4,17 +4,27 @@ var Node = require('node')
 var SP = require('core-front/smartPath/smartPath')
 var IO = require('io')
 var JSONX = require('core-front/jsonx/jsonx')
-var OVL = require('overlay')
 var AD = require('kg/agiledialog/1.0.2/index')
+var AI = require('core-front/authIdentify/index')
 var STB = require('kg/stepbar/2.1.0/index')
 var containerView = require('./container-view')
-var stepBarModule = require('./stepBar/stepBar')
+var stepBar = require('./stepBar/stepBar')
 module.exports = {
     init: function () {
-        var containerTpl = new XTemplate(containerView)
-        var containerHtml = containerTpl.render({})
-        $('article').html(containerHtml)
+        var ai = new AI(token);
+        if (ai.existChecked()) {
+            IO.post(SP.resolvedIOPath('project/getProjectWithBucket?_content=json'), {
+                id: projectId
+            }, function (cb) {
+                var containerTpl = new XTemplate(containerView)
+                var containerHtml = containerTpl.render({})
+                $('article').html(containerHtml)
 
-        stepBarModule.init($('.stepBarContainer'))
+                stepBar.init({
+                    node: $('.stepBarContainer')
+                    , phase: cb.data.phase
+                })
+            }, "json")
+        }
     }
 }
--- src/viewProjectPage/stepBar/stepBar.js
+++ src/viewProjectPage/stepBar/stepBar.js
@@ -8,7 +8,7 @@ module.exports = {
     init: function (p) {
         var tpl = new XTemplate(stepBarVW)
         var html = tpl.render({})
-        p.html(html)
+        p.node.html(html)
         var _step
         var _relation = $({
             editing: 1
@@ -16,9 +16,19 @@ module.exports = {
             , checking: 3
             , pass: 4
         })
+        var step = function (n) {
+            var a = _relation.prop(n)
+            if (a == null) {
+                _step.set('act', -1)
+            }
+            else if (_step.get('act') != a) {
+                _step.set('act', a);
+            }
+        }
         var renderStep = function () {
             _step = new STB('#projectPhase', {'color': 'blue'})
             _step.render()
+            step(p.phase)
         }
         KISSY.use('kg/stepbar/2.1.0/stepbar.css', function (KISSY) {
             KISSY.use('awarding-account-front/projectPage/stepBar/stepBar.css', function (KISSY) {
