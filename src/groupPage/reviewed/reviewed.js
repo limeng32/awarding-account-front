d661e9e0ea74b39c91af2de600278939a4e46cd4
limeng32 <limeng32@live.cn>
2017-01-15 13:02:35 +0800

    增加了reviewProject-view.xtpl 修改了reviewed.js的结构 在xtemplate中使用{{{}}}标签以达到模板嵌套的效果

--- src/groupPage/reviewed/reviewed.js
+++ src/groupPage/reviewed/reviewed.js
@@ -5,19 +5,28 @@ var Node = require('node');
 var SP = require('core-front/smartPath/smartPath')
 var JSONX = require('core-front/jsonx/jsonx')
 var view = require('./reviewed-view');
+var projectView = require('./reviewedProject-view');
 module.exports = {
     init: function (p) {
-        var tpl = new XTemplate(view)
-        IO.post(SP.resolvedIOPath('group/listProject?_content=json'),
-            {
-                phase: 'submited'
-            },
+        var tpl = new XTemplate(view), projectTpl = new XTemplate(projectView)
+        IO.post(SP.resolvedIOPath('group/listCompanyType?_content=json'),
+            {},
             function (d) {
                 d = JSONX.decode(d)
-                p.node.html(tpl.render({
-                    data: d.data
-                }))
+                var projectHtml
+                IO.post(SP.resolvedIOPath('group/listProject?_content=json'),
+                    {
+                        phase: 'submited'
+                    },
+                    function (d2) {
+                        projectHtml = projectTpl.render({
+                            data: d2.data
+                        })
+                        p.node.html(tpl.render({
+                            projectHtml: projectHtml
+                        }))
+                    }, "json")
+                console.log(d.data)
             }, "json")
-
     }
 }
