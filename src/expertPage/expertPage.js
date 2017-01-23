b03ee9e5de7b97d3819a0c2f1cdd09950ec77412
li3081934 <jie3081934@126.com>
2017-01-23 14:36:33 +0800

    添加专家栏目  邀请专家页

--- src/expertPage/expertPage.js
+++ src/expertPage/expertPage.js
@@ -1,9 +1,24 @@
+/*
 KISSY.add('awarding-account-front/groupPage/groupPage', ["awarding-account-front/homePage/header", "awarding-account-front/groupPage/article", "core-front/smartPath/smartPath"], function (S, require, exports, module) {
     KISSY.use('awarding-account-front/groupPage/groupPage.css', function (S) {
         var header = require('awarding-account-front/homePage/header');
         header.init('home_u18');
 
         var article = require('awarding-account-front/groupPage/article');
+        article.init();
+
+        var SP = require('core-front/smartPath/smartPath');
+        SP.resolveImgSrc('.img');
+    })
+});
+*/
+
+KISSY.add('awarding-account-front/groupPage/groupPage.css', ["awarding-account-front/homePage/header", "awarding-account-front/expertPage/article", "core-front/smartPath/smartPath"], function (S, require, exports, module) {
+    KISSY.use('awarding-account-front/groupPage/groupPage.css', function (S) {
+        var header = require('awarding-account-front/homePage/header');
+        header.init('home_u18');
+
+        var article = require('awarding-account-front/expertPage/article');
         article.init();
 
         var SP = require('core-front/smartPath/smartPath');
